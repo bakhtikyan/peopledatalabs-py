3590c4199c316352d635dfa657b2d41488b6c76d
Igor Bakhtikyan <igor.bakhtikyan@toptal.com>
2022-03-23 19:14:15 +0400

    Update setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pypdl',
     version='0.0.1',
     description='People Data Labs Python Library',
     url='https://github.com/bakhtikyan/peopledatalabs-py',
-    packages=['pypdl'],
+    packages=find_packages(),
     install_requires=[
                         'pytest',
                         'requests',
